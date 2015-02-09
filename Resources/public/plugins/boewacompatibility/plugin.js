6c2c6fbc448a16db55be8c8ba0ad9de91d3d2370
Jannik <j.waschkau@boewa.de>
2015-02-09 17:35:37 +0100

    updated compatibility package

--- Resources/public/plugins/boewacompatibility/plugin.js
+++ Resources/public/plugins/boewacompatibility/plugin.js
@@ -5,14 +5,19 @@ CKEDITOR.plugins.add( 'boewacompatibility', {
     init: function( editor ) {
         editor.on('instanceReady', function() {
             $(window).trigger('resize');
-
+            jQuery( editor.element.$ ).on('remote.replace.content', function() {
+                    //Damit der Editor sich updated wenn das unterliegende Element verändert wird
+                    console.log(jQuery( editor.element.$).html());
+                    editor.setData(jQuery( editor.element.$).html());
+                }
+            );
             //FIXME: does not work like this, because the editor is inside an iframe
             $("#form",parent.document).on('form-pre-serialize',function(){
-            if (typeof CKEDITOR !== 'undefined') {
-                for ( instance in CKEDITOR.instances ) {
-                    CKEDITOR.instances[instance].updateElement();
+                if (typeof CKEDITOR !== 'undefined') {
+                    for ( instance in CKEDITOR.instances ) {
+                        CKEDITOR.instances[instance].updateElement();
+                    }
                 }
-            }
             });
         });
         editor.on('resize', function() {
